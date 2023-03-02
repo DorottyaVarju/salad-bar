cebb3fb903830ae337db40fe281b05d558bf4a0b
DorottyaVarju <varjudorottya@gmail.com>
2023-03-02 22:46:28 +0100

    I added map to the page and some css code

---
+++ contact.php
@@ -0,0 +1,39 @@
+<?php
+require_once('header.php');
+?>
+<link rel="stylesheet" href="css/contact.css" type="text/css" />
+
+<main class="container-fluid">
+  <section class="col-sm-12 contact">
+    <div>
+      <h2>Contact Us</h2>
+      <ul>
+        <li>Salad Bar <i class="fa fa-leaf"></i></li>
+        <li>123 Main Street</li>
+        <li>Anytown, USA 12345</li>
+        <li>Phone: (555) 555-1234</li>
+        <li>Email: info@saladbar.com</li>
+      </ul>
+    </div>
+    <br /><br /><br />
+    <div>
+      <h2>OPENING HOURS</h2>
+      <ul>
+        <li>Monday - Saturday: 9:00 AM - 10:00 PM</li>
+        <li>On Sunday, CLOSED</li>
+      </ul>
+    </div>
+  </section>
+  <section class="col-sm-12 contact">
+    <iframe
+      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13007072.762046704!2d-104.65405328265224!3d37.25826730064588!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54eab584e432360b%3A0x1c3bb99243deb742!2zRWd5ZXPDvGx0IMOBbGxhbW9r!5e0!3m2!1shu!2shu!4v1677792216673!5m2!1shu!2shu"
+      style="border: 0"
+      allowfullscreen=""
+      loading="lazy"
+      referrerpolicy="no-referrer-when-downgrade"
+    ></iframe>
+  </section>
+</main>
+<?php
+require_once "footer.php";
+?> 
