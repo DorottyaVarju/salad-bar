e73cac83dca4bfc3bf71945a96678b41de9cc447
DorottyaVarju <varjudorottya@gmail.com>
2023-03-02 20:09:11 +0100

    I modified the css of the menu, changed font size and others, added scrollbar to the menu, and changed the css of the index page

--- header.php
+++ header.php
@@ -22,13 +22,12 @@
     />
     <link
       rel="stylesheet"
-      href="https://fonts.googleapis.com/css?family=Audiowide"
+      href="https://fonts.googleapis.com/css?family=Lobster"
     />
-    <link rel="stylesheet" href="css/menu.css" type="text/css" />
+    <link rel="stylesheet" href="css/header.css" type="text/css" />
     <link rel="stylesheet" href="css/home.css" type="text/css" />
-    <link rel="stylesheet" href="css/salads.css" type="text/css" />
     <title>Salad Bar</title>
-    <link rel="icon" type="image/x-icon" href="media/image/favicon.png">
+    <link rel="icon" type="image/x-icon" href="media/image/leaf.png" />
     <?php require_once('dbConnection.php');?>
   </head>
   <body>
@@ -46,14 +45,28 @@
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="collapsibleNavbar">
-          <ul class="navbar-nav">
-            <li class="nav-item"><a class="nav-link" href="salads.php">Salads</a></li>
-            <li class="nav-item"><a class="nav-link" href="smoothies.php">Smoothies</a></li>
-            <li class="nav-item"><a class="nav-link" href="soups.php">Soups</a></li>
-            <li class="nav-item"><a class="nav-link" href="aboutus.php">About Us</a></li>
-            <li class="nav-item"><a class="nav-link" href="contact.php">Contact</a></li>
-            <li class="nav-item"><a class="nav-link" href="login.php">Log In</a></li>
+          <ul class="navbar-nav"> 
+            <li class="nav-item">
+              <a class="nav-link" href="salads.php">Salads</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="smoothies.php">Smoothies</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="soups.php">Soups</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="aboutus.php">About Us</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="contact.php">Contact</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="loginSignup.php">Log In</a>
+            </li>
           </ul>
         </div>
       </div>
     </nav>
+  </body>
+</html>
--- index.php
+++ index.php
@@ -1,43 +1,35 @@
 <?php require_once('header.php');?>
 
-    <main class="container-fluid">
-      <section class="col-lg-6">
-        <h1>
-          Welcome to <span>Salad Bar <i class="fa fa-leaf"></i></span>!
-        </h1>
-        <br />
-        <p>
-          We're excited to bring you the freshest and healthiest salads around!
-        </p>
-        <p>
-          Our mission is to provide you with a wide variety of fresh greens,
-          toppings, and dressings so that you can create your own perfect salad
-          every time. We believe that eating healthy shouldn't be boring or
-          bland. That's why we only use the freshest, locally sourced
-          ingredients to ensure that every bite is bursting with flavor. Our
-          salad bar is fully stocked with everything you need to create your
-          perfect salad.
-        </p>
-        <p>
-          From crisp greens and colorful veggies to a variety of proteins and
-          dressings, we've got you covered. And if you're not sure where to
-          start, our friendly staff is always happy to make recommendations or
-          offer guidance. We take pride in offering a healthy and delicious
-          alternative to fast food, so come on in and discover your new favorite
-          salad.
-        </p>
-        <p>
-          Thank you for visiting our website, and we can't wait to see you in
-          person!
-        </p>
-      </section>
+<main class="container-fluid">
+  <section class="col-lg-6">
+    <h1>
+      Welcome to <span>Salad Bar <i class="fa fa-leaf"></i></span>!
+    </h1>
+    <br />
+    <p>We're excited to bring you the freshest and healthiest salads around!</p>
+    <p>
+      We believe that eating healthy shouldn't be boring or bland. That's why we
+      only use the freshest, locally sourced ingredients to ensure that every
+      bite is bursting with flavor.
+    </p>
+    <p>
+      We believe that healthy eating should be easy and delicious! Our menu is
+      designed to offer a wide variety of fresh, nutritious salads that are
+      perfect for busy people on the go. Whether you're looking for a quick
+      lunch, a post-workout snack, or a tasty dinner option, we've got you
+      covered. So come on in, choose your favorite salad, and let us take care
+      of the rest!
+    </p>
+    <p>
+      Thank you for visiting our website, and we can't wait to see you in
+      person!
+    </p>
+  </section>
 
-      <section class="col-lg-6">
-        <img src="media/image/products/soup/gingercarrot.jpg" alt="favourites" />
-        <br />
-        <br />
-        <br />
-        <p>Favourites of the week</p>
-      </section>
-    </main>
-    <?php require_once('footer.php');?>
+  <section class="col-lg-6">
+    <video width="640" height="480" autoplay loop>
+      <source src="media/video/vegetables.mp4" type="video/mp4" />
+    </video>
+  </section>
+</main>
+<?php require_once('footer.php');?>
