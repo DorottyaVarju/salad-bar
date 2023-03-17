cd7673be9ad20e1830059a874791aa25681cb82c
DorottyaVarju <varjudorottya@gmail.com>
2023-03-17 14:44:29 +0100

    there was a problem with adding prices, fixed this problem

--- js/display.js
+++ js/display.js
@@ -11,11 +11,11 @@ function getCookie(name) {
 let productData;
 let tr, td;
 let totalQuantity = 0,
-  totalPrice = 0;
+  price;
+totalPrice = 0;
 const tbody = document.getElementsByTagName("tbody");
 
 console.log(getCookie("productsInBasket"));
-
 
 productData = getCookie("productsInBasket").split(",");
 console.log(productData);
@@ -33,17 +33,17 @@ for (i = 1; i < productData.length; i += 4) {
       totalQuantity += Number(productData[i + 2]);
     } else if (j == 2) {
       td.innerHTML =
-        "<select class='form-select'><option>1</option><option>2</option><option>3</option><option>4</option><option>5</option><option>6</option><option>7</option><option>8</option><option>9</option><option>10</option></select> <button class='btn btn-success add'>Add</button>";
+        "<select class='form-select'><option selected>1</option><option>2</option><option>3</option><option>4</option><option>5</option><option>6</option><option>7</option><option>8</option><option>9</option><option>10</option></select> <button class='btn btn-success add'>Add</button>";
     } else if (j == 3) {
       td.innerHTML = "<i class='fa fa-trash'></i>";
     } else if (j == 4) {
-      td.innerText = productData[i + 1] + "$";
-      totalPrice += Number(productData[i + 1]);
+      price = (productData[i + 1] * productData[i + 2]).toFixed(2);
+      td.innerText = price + "$";
+      totalPrice += Number(price);
     }
     tr.appendChild(td);
   }
 }
-
 
 tr = document.createElement("tr");
 tbody[0].appendChild(tr);
