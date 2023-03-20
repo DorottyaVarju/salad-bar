bfbc9d4983049518f49972ac9c0f3f31da7f1126
DorottyaVarju <varjudorottya@gmail.com>
2023-03-20 20:43:18 +0100

    I made that let the orders page show the user's previous orders

--- pages/orders.php
+++ pages/orders.php
@@ -1,9 +1,13 @@
-<?php require_once "./header.php"; ?>
+<?php require_once "./header.php";
+require_once "../database/dbConnection.php";
+?>
+
 <link rel="stylesheet" href="/salad-bar/css/orders.css" type="text/css" />
 
 <main class="container">
 <table class="table table-condensed table-borderless" >
     <thead>
+      <tr><th></th><th>Your Previous Orders</th><th></th></tr>
       <tr>
         <th>Order ID</th>
         <th>Sum</th>
@@ -11,23 +15,36 @@
       </tr>
     </thead>
     <tbody>
-      <tr>
-        <td>2</td>
-        <td>10.92$</td>
-        <td>delievered</td>
-      </tr>
-      <tr>
-        <td>52</td>
-        <td>8.43$</td>
-        <td>delievered</td>
-      </tr>
-      <tr>
-        <td>61</td>
-        <td>9.04$</td>
-        <td>pending</td>
-      </tr>
+      <?php
+      $id = $_SESSION["id"];
+      try {
+        $handle = $conn->prepare(
+          "SELECT id, recent_status, sum FROM orders WHERE userID=:id"
+        );
+        $params = [
+          ":id" => $id,
+        ];
+
+        $handle->execute($params);
+
+        $result = $handle->fetchAll(PDO::FETCH_ASSOC);
+
+        foreach ($result as $r) {
+          echo "<tr><td>" .
+            $r["id"] .
+            "</td><td>" .
+            $r["sum"] .
+            "</td><td>" .
+            $r["recent_status"] .
+            "  </td></tr>";
+        }
+      } catch (Exception $e) {
+        echo $e->getMessage();
+      }
+      ?>
     </tbody>
   </table>
+  <br> <br> <br> <br>
 </main>
 
 <?php require_once "./footer.php"; ?>
