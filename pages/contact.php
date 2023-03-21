1885d32a9e7ed2fb016190759ec4b28a67fdf265
DorottyaVarju <varjudorottya@gmail.com>
2023-03-21 20:19:23 +0100

    I made the google maps iframe bigger

--- pages/contact.php
+++ pages/contact.php
@@ -3,7 +3,7 @@ require_once "./header.php"; ?>
 <link rel="stylesheet" href="../css/contact.css" type="text/css" />
 
 <main class="container-fluid">
-  <section class="col-sm-12 contact">
+  <section class="col-lg-6 contact">
     <div>
       <h2>Contact Us</h2>
       <ul class="contactData">
@@ -23,14 +23,8 @@ require_once "./header.php"; ?>
       </ul>
     </div>
   </section>
-  <section class="col-sm-12 contact">
-    <iframe
-      src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13007072.762046704!2d-104.65405328265224!3d37.25826730064588!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x54eab584e432360b%3A0x1c3bb99243deb742!2zRWd5ZXPDvGx0IMOBbGxhbW9r!5e0!3m2!1shu!2shu!4v1677792216673!5m2!1shu!2shu"
-      style="border: 0"
-      allowfullscreen=""
-      loading="lazy"
-      referrerpolicy="no-referrer-when-downgrade"
-    ></iframe>
+  <section class="col-lg-6 contact map-responsive">
+  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11707.145463505369!2d-73.99021255498009!3d42.81400036654304!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89de6e0621c7f4cb%3A0x8f40d3dadc5d58e5!2sSchenectady%2C%20New%20York%2012345%2C%20Egyes%C3%BClt%20%C3%81llamok!5e0!3m2!1shu!2shu!4v1679421393087!5m2!1shu!2shu" width="600" height="400" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
   </section>
 </main>
 
