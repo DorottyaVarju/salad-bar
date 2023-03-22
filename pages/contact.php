25652b9f13b57edd5d6c29e4718e44a6bbf97366
DorottyaVarju <varjudorottya@gmail.com>
2023-03-22 15:44:14 +0100

    I changed the language of the embed google maps to english

--- pages/contact.php
+++ pages/contact.php
@@ -24,7 +24,7 @@ require_once "./header.php"; ?>
     </div>
   </section>
   <section class="col-lg-6 contact map-responsive">
-  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11707.145463505369!2d-73.99021255498009!3d42.81400036654304!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89de6e0621c7f4cb%3A0x8f40d3dadc5d58e5!2sSchenectady%2C%20New%20York%2012345%2C%20Egyes%C3%BClt%20%C3%81llamok!5e0!3m2!1shu!2shu!4v1679421393087!5m2!1shu!2shu" width="600" height="400" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d11707.145463505369!2d-73.99021255498009!3d42.81400036654304!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89de6e0621c7f4cb%3A0x8f40d3dadc5d58e5!2sSchenectady%2C%20New%20York%2012345%2C%20Egyes%C3%BClt%20%C3%81llamok!5e0!3m2!1sen!2sen!4v1679421393087!5m2!1sen!2sen" width="600" height="400" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
   </section>
 </main>
 
